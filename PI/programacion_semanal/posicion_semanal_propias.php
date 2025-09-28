df89a68200864d04554684b4e6bd10f40f7e54ae
Juan Felipe Benitez Ramos <juanfelipebenitezramos@MacBook-Air-2.local>
2025-09-27 23:49:29 -0500

    refactor: Elimina directorio obsoleto PI/
    Se elimina el directorio 'PI/' completo por ser considerado obsoleto y no formar parte de la aplicación principal 'construccion'. Esta acción reduce la complejidad y el código a mantener.

--- PI/programacion_semanal/posicion_semanal_propias.php
+++
@@ -1,6 +0,0 @@
-<?php session_start();
-$_SESSION["posicion_semanal"]=$_GET["posicion_semanal"];
-//echo $_SESSION["posicion_intermedia"];
-header("Location: programacion_semanal_propias.php");
-
-?>
--- PI/scroll_general.php
+++
@@ -1,8 +0,0 @@
-<?php session_start();
-$_SESSION["scroll"]=$_GET["scroll"];
-$seccion=$_SESSION["seccion"];
-$semana=$_SESSION["semana"];
-//echo $_SESSION["scroll"];
-header("Location: cambiar_pagina.php?seccion=$seccion&semana=$semana");
-
-?>
